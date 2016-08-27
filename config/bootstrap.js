8ae82a8f5da5550146c634b32ce6329bad62c35a
David Griffith <davidwaynegriffith@gmail.com>
2016-08-26 21:47:21 -0700

    Data Models, Mock Data in bootstrap.js

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,9 +9,19 @@
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
-module.exports.bootstrap = function(cb) {
+module.exports.bootstrap = function (cb) {
 
-  // It's very important to trigger this callback method when you are finished
-  // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
+  var products = [{ brand: 'Apple', model: 'Macbook Air', price: 1299, description: 'This is a macbook air...' },{ brand: 'Apple', model: 'Macbook Pro', price: 1999, description: 'This is a macbook pro...' },{ brand: 'Apple', model: 'iPad Pro', price: 1299, description: 'This is an iPad Pro...' }];
+  var reviews = [{ title: 'Review of the Apple Macbook Air', author: 'David G', body: 'This macbook air is the greatest thing...' }];
+
+  Product.create(products).exec(function (err, products) {
+    if (err) console.log(err);
+    console.log('Products created:', products);
+    Review.create(reviews).exec(function (err, reviews) {
+      if (err) console.log(err);
+      console.log('Reviews created:', reviews);
+      cb();
+    });
+  });
+
 };
